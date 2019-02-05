f66865413c12e137b3da7d9e0c97c3fb7719f8ce
Taylor Williams <taylor@thewilliams.net>
2019-02-05 08:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ class BoardMember{
   veto() {
     return 'No, I must disagree'
   }
-  approve {
+  approve() {
     return 'You can do that!'
   }
-  doCharity {
+  doCharity() {
     return "I like to help people."
   }
 
-  releasePressStatement {
+  releasePressStatement() {
     return "You will see great things from Scuber."
   }
 
-  sayHi {
+  sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
